0573b799ee64e0a9e458d97c647835dc318b6f09
Matheus Vinagre <matheus.vinagre@hotmail.com>
2024-08-15 16:35:56 -0300

    creating carrossel

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 export function Header(){
     return (
-            <div className="h-24 flex justify-between items-center bg-blue-950 px-8">
+            <div className="h-24 flex justify-between items-center bg-blue-950 px-28">
                 <img className="h-16 cursor-pointer" src="./assets/logo.png" alt="" />
                 <div className="flex gap-4 font- text-sm text-blue-100 p-2">
                     <a className="text-center hover:font-bold" href="">HOME</a> <p className="text-zinc-500">|</p>
--- src/components/slide.tsx
+++ src/components/slide.tsx
@@ -4,16 +4,22 @@ import { Navigation, Pagination } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/navigation'
+import { Autoplay } from 'swiper/modules';
 
   
 export function Carousel(){
     return (
       <Swiper
+        spaceBetween={50}
         slidesPerView={1}
         navigation
-        modules={[Navigation, Pagination]}
+        modules={[Navigation, Pagination, Autoplay]}
         pagination={{ clickable: true }}
         loop={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: true,
+        }}
       >
         <SwiperSlide><img src="./assets/image.png" alt="asdasd" /></SwiperSlide>
         <SwiperSlide><img src="./assets/slide2.jpg" alt="sadsad" /></SwiperSlide>
