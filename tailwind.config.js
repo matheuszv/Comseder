de199efa9f78ba3148374ffe3d25ce7091a91222
Matheus Vinagre <matheus.vinagre@hotmail.com>
2024-08-15 17:32:28 -0300

    bgsfd

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      backgroundImage: {
+        'custom-image': "url('fundo.png')",
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
