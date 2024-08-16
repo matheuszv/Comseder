dfecb7fc7cecdb06bf14e64b43dcf794eb781b52
Matheus Vinagre <matheus.vinagre@hotmail.com>
2024-08-16 11:56:18 -0300

    Bg-branco

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -28,7 +28,7 @@ export function Home(){
                 <div className="h-px w-full bg-sky-300"/>
             </div>
 
-            <div className="justify-center flex flex-col mt-6 items-center">
+            <div className="justify-center flex flex-col mt-6 items-center bg-white">
                 <h3 className="text-zinc-800 font-bold text-3xl pb-2">Comunicados</h3>
                 <span className="text-sky-500 text-sm hover:text-sky-700 hover:underline h-7"><a href="#">Ver mais comunicados</a></span>
                 <div className="justify-around items-center flex gap-10 px-10 mt-8">
