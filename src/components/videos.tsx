0617fd689a486d96a31d0f78c69799ac88f0e4da
Matheus Vinagre <matheus.vinagre@hotmail.com>
2024-08-16 00:22:34 -0300

    video melhoras

--- src/components/videos.tsx
+++ src/components/videos.tsx
@@ -2,7 +2,7 @@ import meuVideo from '../../public/assets/video-unidas.mp4'
 
 export function VideoPlayer() {
   return (
-    <div className="p-10">
+    <div className="px-60 py-12">
       <video controls width="100%">
         <source src={meuVideo} type="video/mp4" />
         Seu navegador não suporta a tag de vídeo.
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,14 +6,24 @@ export function Home(){
     return(
         <>
             <Carousel/>
-            <div className="justify-center gap-x-20 gap-y-12 flex-wrap flex py-6">
+            <div className="justify-center gap-x-20 gap-y-12 flex-wrap flex py-12">
                 <Cards img="blabla.png">Planos</Cards>
                 <Cards img="blabla.png">Ambulatórios</Cards>
                 <Cards img="blabla.png">Serviços On-line</Cards>
                 <Cards img="blabla.png">Guia Médico</Cards>
             </div>
-            <div className="w-full h-px bg-sky-400 my-10"/>
-            <VideoPlayer />
+
+            <div className="w-full h-px bg-sky-400"/>
+
+            <div className="flex flex-col items-center justify-center my-12">
+                <h3 className="text-zinc-800 font-bold text-xl">
+                    <span className="text-sky-500">COMSEDER </span>
+                    FILIADA A 
+                    <span className="text-blue-800"> UNIDAS</span>
+                </h3>
+                <p className="text-zinc-600 italic">Juntos somos mais fortes</p>
+                <VideoPlayer />
+            </div>
             
         </> 
     )
