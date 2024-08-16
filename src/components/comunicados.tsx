3113115226e68176eb3a2d9d29aead3d63831a8b
Matheus Vinagre <matheus.vinagre@hotmail.com>
2024-08-16 11:45:11 -0300

    Comunicados

--- src/components/comunicados.tsx
+++ src/components/comunicados.tsx
@@ -3,7 +3,7 @@ import { Clock } from 'lucide-react'
 export function Comunicados(){
     return(
         <a href="#">
-            <div className="text-sky-500 bg-sky-100 rounded-lg justify-center items-center flex-col py-4 px-6 w-72 h-76 shadow-xl">
+            <div className="text-sky-500 bg-sky-100 rounded-lg border-transparent border justify-center items-center flex-col py-4 px-6 w-72 h-76 shadow-xl hover:border hover:border-sky-500">
                 <h4 className="text-xl text-left py-3">Título do comunicado</h4>
                 <p className="text-sky-950 text-base pb-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis tempora facilis eligendi cum esse, 
                 aperiam doloribus soluta at architecto id delectus ut sint laboriosam accusamus libero excepturi quod rem impedit.</p>
