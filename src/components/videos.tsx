91b5ceae998ad5ebbeaedf31abcedafc1d402108
Matheus Vinagre <matheus.vinagre@hotmail.com>
2024-08-16 00:33:31 -0300

    video corrected

--- src/components/videos.tsx
+++ src/components/videos.tsx
@@ -2,8 +2,8 @@ import meuVideo from '../../public/assets/video-unidas.mp4'
 
 export function VideoPlayer() {
   return (
-    <div className="px-60 py-12">
-      <video controls width="100%">
+    <div className="px-56 py-6">
+      <video controls autoPlay width="100%">
         <source src={meuVideo} type="video/mp4" />
         Seu navegador não suporta a tag de vídeo.
       </video>
