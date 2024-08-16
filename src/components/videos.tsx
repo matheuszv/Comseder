f55769d8eb95df881e94c22aba10c99c076640ad
Matheus Vinagre <matheus.vinagre@hotmail.com>
2024-08-16 12:04:01 -0300

    shadow-video

--- src/components/videos.tsx
+++ src/components/videos.tsx
@@ -3,7 +3,7 @@ import meuVideo from '../../public/assets/video-unidas.mp4'
 export function VideoPlayer() {
   return (
     <div className="px-40 py-6">
-      <video controls autoPlay width="100%">
+      <video controls autoPlay width="100%" className="shadow-xl">
         <source src={meuVideo} type="video/mp4" />
         Seu navegador não suporta a tag de vídeo.
       </video>
