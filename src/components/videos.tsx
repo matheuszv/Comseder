8c6dfb33033034b093ad5b1673563bcc12177c39
Matheus Vinagre <matheus.vinagre@hotmail.com>
2024-08-16 10:25:11 -0300

    mudanças fundo e video

--- src/components/videos.tsx
+++ src/components/videos.tsx
@@ -2,7 +2,7 @@ import meuVideo from '../../public/assets/video-unidas.mp4'
 
 export function VideoPlayer() {
   return (
-    <div className="px-56 py-6">
+    <div className="px-40 py-6">
       <video controls autoPlay width="100%">
         <source src={meuVideo} type="video/mp4" />
         Seu navegador não suporta a tag de vídeo.
