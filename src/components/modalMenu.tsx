a00ba65770c53ceaeff0eaeeb677bc0da4635b37
Matheus Vinagre <matheus.vinagre@hotmail.com>
2024-08-17 23:12:39 -0300

    Olha olhada nesse modal

--- src/components/modalMenu.tsx
+++ src/components/modalMenu.tsx
@@ -5,9 +5,17 @@ interface ModalMenu{
 
 export function ModalMenu({closeMenuModal}: ModalMenu){
     return(
-        <div className="flex justify-end">
-            <div className="bg-sky-700 flex flex-col w-8/12 justify-center">
+        <div className="fixed inset-0 bg-black/60 flex justify-end">
+            <div className="bg-sky-700 flex flex-col w-8/12 justify-center items-center">
                 <button onClick={closeMenuModal}>FECHA</button>
+                    <a className="hover:font-bold" href={`/`}>HOME</a>
+                    <a className="hover:font-bold" href={`/institucional`}>INSTITUCIONAL</a>
+                    <a className="hover:font-bold" href="">AMBULATÓRIO</a>
+                    <a className="hover:font-bold" href="">PLANOS</a>
+                    <a className="hover:font-bold" href="">SERVIÇOS</a>
+                    <a className="hover:font-bold" href="">PRESTADOR</a>
+                    <a className="hover:font-bold" href="">CONTATO</a>
+                    <a className="hover:font-bold" href="">ANS</a>
             </div>
         </div>
     )
