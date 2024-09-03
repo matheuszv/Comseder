966a6ba63db5a8170fdd88d59d184174c6ecd950
Matheus Vinagre <matheus.vinagre@hotmail.com>
2024-09-02 23:54:59 -0300

    block

--- src/pages/planos/index.tsx
+++ src/pages/planos/index.tsx
@@ -18,7 +18,7 @@ export function Planos(){
                 <h2 className="text-sky-900 font-bold text-lg text-center px-12 pt-2">Informações uteis</h2>
                 <div onClick={toggleAccordion} className="flex cursor-pointer bg-sky-900 justify-between items-center p-3 font-normal rounded-md text-sky-50">
                     <h3 className="text-sky-50 font-bold">Como aderir ou mudar o plano</h3> {isOpen? (<X/>) : (<ChevronDown />)}
-                    {isOpen && ( <div className="border border-sky-600 bg-sky-100 px-3 text-zinc-800 py-2"> zap </div>)}
+                    {isOpen && ( <div className="border border-sky-600 bg-sky-100 px-3 text-zinc-800 py-2 block"> zap </div>)}
                 
                 </div>   
             </div>
