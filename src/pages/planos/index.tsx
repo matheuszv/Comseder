6ba519bebd53155b8d545b204e153c6acf0ebd26
Matheus Vinagre <matheus.vinagre@hotmail.com>
2024-09-02 23:58:20 -0300

    sdfs

--- src/pages/planos/index.tsx
+++ src/pages/planos/index.tsx
@@ -16,9 +16,9 @@ export function Planos(){
         <div className="flex gap-16 flex-wrap px-24">
             <div className="flex-1 space-y-8 bg-white px-8 mb-10 rounded-3xl shadow-xl">
                 <h2 className="text-sky-900 font-bold text-lg text-center px-12 pt-2">Informações uteis</h2>
-                <div onClick={toggleAccordion} className="flex cursor-pointer bg-sky-900 justify-between items-center p-3 font-normal rounded-md text-sky-50">
+                <div onClick={toggleAccordion} className="flex cursor-pointer bg-sky-900 justify-between items-center p-3 font-normal rounded-md text-sky-50 flex-col">
                     <h3 className="text-sky-50 font-bold">Como aderir ou mudar o plano</h3> {isOpen? (<X/>) : (<ChevronDown />)}
-                    {isOpen && ( <div className="border border-sky-600 bg-sky-100 px-3 text-zinc-800 py-2 block">
+                    {isOpen && ( <div className="border border-sky-600 bg-sky-100 px-3 text-zinc-800 py-2">
                          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error quas sequi nam fuga soluta possimus, ex, adipisci ea architecto ad veritatis? Consequatur vitae temporibus nulla dicta deleniti? Repellendus, repellat! Alias!</p> 
                     </div>)}
                 
