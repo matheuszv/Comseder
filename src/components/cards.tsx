95c5451887a9ceab9576183a90d4b2590afabf2f
Matheus Vinagre <matheus.vinagre@hotmail.com>
2024-09-06 17:16:53 -0300

    bg-cards-white

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -5,7 +5,7 @@ interface Cards{
 
 export function Cards({children, img}:Cards){
     return(
-        <div className="p-8 w-48 justify-center items-center flex flex-col cursor-pointer border border-transparent font-semibold bg-sky-50 rounded-lg shadow-lg hover:border-sky-600 hover:border hover:text-sky-600">
+        <div className="p-8 w-48 justify-center items-center flex flex-col cursor-pointer border border-transparent font-semibold bg-white rounded-lg shadow-lg hover:border-sky-600 hover:border hover:text-sky-600">
             <img className="h-20 w-20" src={img} alt="icone"/>
             <h3 className="my-3">{children}</h3>
         </div>
