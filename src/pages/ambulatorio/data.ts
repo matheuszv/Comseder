f18f9d4f36fe26fbb7a0982f0a0ae3d689dbbaff
Matheus Vinagre <matheus.vinagre@hotmail.com>
2024-09-13 19:44:42 -0300

    fade

--- src/pages/ambulatorio/data.ts
+++ src/pages/ambulatorio/data.ts
@@ -4,34 +4,34 @@ export const projects = [
       description: "Originally hailing from Austria, Berlin-based photographer Matthias Leindinger is a young creative brimming with talent and ideas.",
       src: "assets/gineco.jpg",
       link: "https://www.ignant.com/2023/03/25/ad2186-matthias-leidingers-photographic-exploration-of-awe-and-wonder/",
-      color: "#BBACAF"
+      color: "#06b6d4"
     },
     {
       title: "Geriatria",
       description: "This is a story on the border between reality and imaginary, about the contradictory feelings that the insularity of a rocky, arid, and wild territory provokes”—so French photographer Clément Chapillon describes his latest highly captivating project Les rochers fauves (French for ‘The tawny rocks’).",
       src: "assets/geria.jpg",
       link: "https://www.ignant.com/2022/09/30/clement-chapillon-questions-geographical-and-mental-isolation-with-les-rochers-fauves/",
-      color: "#977F6D"
+      color: "#0891b2"
     },
     {
       title: "Enfermaria",
       description: "Though he views photography as a medium for storytelling, Zissou’s images don’t insist on a narrative. Both crisp and ethereal, they’re encoded with an ambiguity—a certain tension—that lets the viewer find their own story within them.",
       src: "assets/enferma.jpg",
       link: "https://www.ignant.com/2023/10/28/capturing-balis-many-faces-zissou-documents-the-sacred-and-the-mundane-of-a-fragile-island/",
-      color: "#C2491D"
+      color: "#0e7490"
     },
     {
         title: "Dermatologia",
         description: "This is a story on the border between reality and imaginary, about the contradictory feelings that the insularity of a rocky, arid, and wild territory provokes”—so French photographer Clément Chapillon describes his latest highly captivating project Les rochers fauves (French for ‘The tawny rocks’).",
         src: "assets/dermato.jpg",
         link: "https://www.ignant.com/2022/09/30/clement-chapillon-questions-geographical-and-mental-isolation-with-les-rochers-fauves/",
-        color: "#BBACAF"
+        color: "#155e75"
     },
     {
         title: "Fisioterapia",
         description: "This is a story on the border between reality and imaginary, about the contradictory feelings that the insularity of a rocky, arid, and wild territory provokes”—so French photographer Clément Chapillon describes his latest highly captivating project Les rochers fauves (French for ‘The tawny rocks’).",
         src: "assets/fisio.jpg",
         link: "https://www.ignant.com/2022/09/30/clement-chapillon-questions-geographical-and-mental-isolation-with-les-rochers-fauves/",
-        color: "#C2491D"
+        color: "#164e63"
     },
 ]
